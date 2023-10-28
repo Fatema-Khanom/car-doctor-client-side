2360cb676f06e0e54d2b0799afc924c8164b3201
Fatema Khanom <fatemaurmi2019@gmail.com>
2023-10-28 19:25:46 +0600

    Services section added

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Bannar from './Bannar/Bannar';
 import AboutUs from './AboutUs/AboutUs';
+import Services from './Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
             <AboutUs></AboutUs>
+            <Services></Services>
         </div>
     );
 };
